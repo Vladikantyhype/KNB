19ed124b9955b09b42b9f6ca9377415f003c2aba
Vladikantyhype <martynenko.vladislav@mail.ru>
2020-05-20 22:24:07 +0300

    Final

--- JSfile.js
+++ JSfile.js
@@ -1,61 +1,74 @@
+const paper = "paper";
+const rock = "rock";
+const scissors = "scissors";
+const choices = [paper, rock, scissors];
+
 function computerPlay() {
-    var selectionArray = ["rock", "paper", "scissors"];
-    var selection = selectionArray[Math.floor(Math.random() * 3)];
+    var i = Math.floor(Math.random() * 3);
+    return choices[i];
+}
+let userScore = 0;
+let compScore = 0;
 
-    var extension = ''
-    if(selection == 'paper') {
-        extension = '.jfif'
-    } else if (selection == 'rock') {
-        extension = '.jpg'
-    } else {
-        extension = '.png'
+
+function game(playerChoices, computerChoices ) {
+    const result = document.getElementById("txtResult");
+
+    if (playerChoices == paper){
+        if (computerChoices == paper){
+            result.innerText = "Ничья!";
+        }
+        if (computerChoices == rock){
+            result.innerText = "Победа! Бумага накрывает камень!";
+            userScore ++;
+        }
+        if (computerChoices == scissors){
+            result.innerText = "Поражение! Ножницы режут бумагу!";
+            compScore ++;
+        }
     }
+    if (playerChoices == rock){
+        if (computerChoices == rock){
+            result.innerText = "Ничья!";
+        }
+        if (computerChoices == scissors){
+            result.innerText = "Победа! Камень ломает ножницы!";
+            userScore ++;
+        }
+        if (computerChoices == paper){
+            result.innerText = "Поражение! Бумага накрывает камень!";
+            compScore ++;
+        }
+    }
+    if (playerChoices == scissors){
+        if (computerChoices == scissors){
+            result.innerText = "Ничья!";
+        }
+        if (computerChoices == paper){
+            result.innerText = "Победа! Ножницы режут бумагу!";
+            userScore ++;
+        }
+        if (computerChoices == rock){
+            result.innerText = "Поражение! Камень ломает ножницы!";
+            compScore ++;
+        }
+    }
+    const gamescore = document.getElementById("score").innerText= userScore + " - " + compScore;
 
-    const compImg = document.querySelector('#compSelecImg')
-    compImg.src = `imgs/${selection}${extension}`
-    return selection;
-}
-
-function playRound(e) {
-
-    e.target.classList.add('animated')
-    var playerSelection = e.toElement.id
-    var computerSelection = computerPlay();
-    if (playerSelection == computerSelection) {
-        setMessage("Draw")
-    } else if (playerSelection == "rock" && computerSelection == "paper" ||
-        playerSelection == "paper" && computerSelection == "scissors" ||
-        playerSelection == "scissors" && computerSelection == "rock") {
-        setMessage("Lose")
-        computerScore += 1
-        setScore()
-    } else {
-        setMessage("Win")
-        playerScore += 1
-        setScore()
+    if (userScore == 5){
+        final.innerText="Победа человека!";
+    }
+    else {
+        final.innerText = " ";
+    }
+    if (compScore == 5){
+        final.innerText = "Победа машины";
+    }
+    else if (userScore != 5){
+        final.innerText = " ";
+    }
+    if ((userScore == 5) || (compScore == 5)){
+        userScore = 0;
+        compScore = 0;
     }
 }
-
-function setScore() {
-    score.innerHTML = "Score: " + playerScore + " to " + computerScore
-}
-
-function setMessage(message) {
-    query = document.querySelector('.message h3')
-    query.innerHTML = message
-}
-
-function removeTransition(e) {
-
-    e.target.classList.remove('animated');
-}
-
-
-const btns = Array.from(document.querySelectorAll('.btn'))
-btns.forEach(btn => btn.addEventListener('click', playRound))
-btns.forEach(btn => btn.addEventListener('transitionend', removeTransition))
-
-var computerScore = 0
-var playerScore = 0
-const score = document.querySelector('.score h3')
-setScore()
